93b1d3611322cbfd78ec45c0fd8bc04d74f42e67
timoruslim <timothyniels@gmail.com>
2025-05-20 00:39:43 +0700

    fix: loop test

--- src/main.cpp
+++ src/main.cpp
@@ -11,45 +11,36 @@ int N, M, num_piece;
 
 int main()
 {
-    cout << "Input: " << flush;
-    cin >> N >> M >> num_piece;
-    vector<string> temp_board;
-    for(int i = 0; i < N; i++){
-        string temp_row;
-        cin >> temp_row;
-        temp_board.push_back(temp_row);
-    }
-    Papan board = Papan(temp_board, N, M);
-    // board.movePiece('I',4,4,3,0,-1);
-    board.printGrid();
-    vector<Piece> pieces = board.extractPieces();
-    vector<pair<Piece, Move>> moves;
-    State current_state(board, moves, pieces, 0);
+    while (true) {
+        cout << "Input: " << flush;
+        cin >> N >> M >> num_piece;
 
-    // cout << "\nRunning A* search with Euclidean distance heuristic..." << endl;
-    vector<pair<Piece, Move>> solution = AStar(current_state);
-
-    if (!solution.empty()) {
-        // cout << "\nSolution found in " << solution.size() << " moves:" << endl;
-        for (size_t i = 0; i < solution.size(); i++) {
-            cout << "Move " << (i+1) << ": ";
-            cout << "Piece " << solution[i].first.name << " moves ";
-            cout << solution[i].second.arah << " by " << solution[i].second.dist << endl;
+        vector<string> temp_board;
+        for(int i = 0; i < N; i++){
+            string temp_row;
+            cin >> temp_row;
+            temp_board.push_back(temp_row);
         }
-    } else {
-        cout << "\nNo solution found." << endl;
+
+        Papan board = Papan(temp_board, N, M);
+        vector<Piece> pieces = board.extractPieces();
+        vector<pair<Piece, Move>> moves;
+        State current_state(board, moves, pieces, 0);
+
+        board.printGrid();
+
+        vector<pair<Piece, Move>> solution = AStar(current_state);
+
+        if (!solution.empty()) {
+            for (size_t i = 0; i < solution.size(); i++) {
+                cout << "Move " << (i+1) << ": ";
+                cout << "Piece " << solution[i].first.name << " moves ";
+                cout << solution[i].second.arah << " by " << solution[i].second.dist << endl;
+            }
+        } else {
+            cout << "\nNo solution found." << endl;
+        }
+        cout << "\n"  << endl;
     }
-    // // current_state.printPieces();
-    // current_state.printAllPossibleMoves();
-    // vector<pair<Piece,Move>> gerak = current_state.getAllPossibleMoves();
-    // State state_baru = applyMoveToState(current_state, gerak[5]);
-    // state_baru.papan.printGrid();
-    // state_baru.printAllMoves();
-    // cout << state_baru.computeManhattanDistance();
-    // vector<pair<Piece, Move>> solution = AStarEuclidean(current_state);
-    // for (auto& step : solution) {
-    // step.first.displayPiece();
-    // step.second.displayMove();
-// }
 
 }
